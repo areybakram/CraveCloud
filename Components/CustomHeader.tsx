1809a22dfea7a7d1f04055f2cce8ae61f4cbfe64
areebakram <your_github_email@example.com>
2025-11-19 14:52:40 +0500

    header component done

---
+++ Components/CustomHeader.tsx
@@ -0,0 +1,129 @@
+import { Colors } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { Link } from "expo-router";
+import React from "react";
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SearchBar = () => (
+  <View style={styles.searchContainer}>
+
+    <View style={styles.searchSection}>
+      <View style={styles.searchField}>
+        <Ionicons name="search-outline" size={20} color={Colors.medium} style={{right:10, top:10 , paddingLeft:20}} />
+        <TextInput style={styles.input} placeholder="Restraunts , Dishes , Groceries"></TextInput>
+
+        </View>
+
+      <Link href={"/"} asChild>
+        <TouchableOpacity style={styles.optionButton}>
+          <Ionicons name="options-outline" size={24} color={Colors.primary} />
+        </TouchableOpacity>
+      </Link>
+
+
+
+    </View>
+  </View>
+);
+
+const CustomHeader = () => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <TouchableOpacity>
+          <Image
+            style={styles.bike}
+            source={require("../assets/images/bike.png")}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.locationContainer}>
+          <Text style={styles.locationTitle}>Delivery . Now</Text>
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+            <Text style={styles.subTitle}>Lahore, Pakistan</Text>
+            <Ionicons name="chevron-down" color={Colors.primary} />
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.profileButton}>
+          <Ionicons name="person-outline" size={24} color={Colors.primary} />
+        </TouchableOpacity>
+      </View>
+
+      <SearchBar />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 60,
+    backgroundColor: "white",
+    flexDirection: "row",
+    gap: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+  safeArea: {
+    flex: 1,
+    backgroundColor: "green",
+  },
+  bike: {
+    height: 40,
+    width: 40,
+  },
+
+  locationContainer: {
+    flex: 1,
+  },
+  profileButton: {
+    backgroundColor: Colors.lightGrey,
+    padding: 8,
+    borderRadius: 30,
+  },
+
+  locationTitle: {
+    fontSize: 14,
+    color: Colors.medium,
+  },
+
+  subTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+
+  searchContainer: {
+    height: 55,
+    backgroundColor: "white",
+  },
+
+  searchSection: {
+    flexDirection: "row", 
+    gap:20,
+    paddingHorizontal:15,
+    alignItems:"center",
+  },
+
+  searchField: {
+    flex: 1,
+    backgroundColor: Colors.lightGrey,
+    borderRadius: 10,
+    flexDirection: "row",
+
+  },
+  optionButton:{
+    padding:8,
+    borderRadius:50,
+
+  }, 
+
+  input:{
+    padding:10,
+    color:Colors.mediumDark,
+
+  }
+});
+
+export default CustomHeader;
